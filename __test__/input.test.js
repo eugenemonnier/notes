f7706812b05c20fa1ee77fe780fcc46feb231b6a
Eugene Monnier <eugene.monnier@gmail.com>
2020-02-11 13:09:25 -0800

    updated functionality of notesy. added test cases.

--- __test__/input.test.js
+++ __test__/input.test.js
@@ -15,16 +15,8 @@ minimist.mockImplementation(() => {
 
 
 describe('Input Module', () => {
-  // test if action isn't specified that the default value is null
-  // it('getAction() defaults to null when no action is specified', () => {
-  //   let options = new Input();
-  //   expect(options.getMethod()).toEqual(null);
-  // });
-  // test if invalid value set for action that help function is called
 
-  // test if invalid flag is set that help function is called
-
-  // test if no value is set for payload that help function is called
+  // test if parseInput() returns a properly formed object when given valid data
   it('parseInput() returns a properly formed object', () => {
     const options = new Input();
     const command = options.parseInput({ a: 'this works'});
@@ -32,13 +24,14 @@ describe('Input Module', () => {
     expect(command.payload).toBeDefined();
   })
 
-  // test if valid values for input are entered that note object is created.
+  // test if validate() will accept a properly formed object
   it('validate() respects a properly formed input', () => {
     const options = new Input();
     expect(options.validate()).toBeTruthy();
   });
 
-  it('valid() rejects an improperly formed input', () => {
+  // test if validate() will reject an improperly formed object
+  it('validate() rejects an improperly formed input', () => {
     let options = new Input();
     options.command = {};
     expect(options.validate()).toBeFalsy();
--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -4,20 +4,22 @@ jest.spyOn(global.console, 'log');
 
 
 describe('Notes module', () => {
-// test for execute()
-// make sure if invalid input, execute() does nothing
-it('execute() does nothing when the options are invalid', () => {
-  const willFail = { command: {'x': 'coconut'} };
-  const notes = new Notes(willFail);
-  notes.execute();
-  expect(console.log).not.toHaveBeenCalled;
-});
-it('Notes.prototype.add() can add a note', () => {
-  const action = 'add';
-  const payload = 'works';
-  const notes = new Notes({ command: { action, payload}})
-  notes.execute();
-  expect(console.log).toHaveBeenCalledWith(`Adding note: ${payload}`);
-})
+  // test for execute()
+  // make sure if invalid input, execute() does nothing
+  it('execute() does nothing when the options are invalid', () => {
+    const willFail = { command: {'x': 'coconut'} };
+    const notes = new Notes(willFail);
+    notes.execute();
+    expect(console.log).not.toHaveBeenCalled;
+  });
+
+  // tests if valid input with action add is used, that the add() function gives expected results
+  it('Notes.prototype.add() can add a note', () => {
+    const action = 'add';
+    const payload = 'works';
+    const notes = new Notes({ command: { action, payload}})
+    notes.execute();
+    expect(console.log).toHaveBeenCalledWith(`Adding note: ${payload}`);
+  })
 
 });
