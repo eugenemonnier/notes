36063ea8d10b9dd102b889e15adc990317e66cc3
Eugene Monnier <eugene.monnier@gmail.com>
2020-02-10 20:28:18 -0800

    primary functionality completed

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
-
+// dependencies
 const Input = require('./lib/input');
 const noteEntry = require('./lib/notes');
 
+// validate input and either process if good values else print error message invalid
 const options = new Input();
 options.validate() ? noteEntry.writeOut(options) : help();
 
+// message printed when input isn't valid
 function help() {
   console.log(`
-  
   notes USAGE: notes -add|-a -p '<your note>'
 
     -add | -a - add a note to your notes
